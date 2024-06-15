bcb14c17526109a1e320fd964536fae7b17fc30d
Maja Ferle <maja.ferle@in516ht.com>
2024-06-15 12:46:14 +0200

    add use warehouse

--- Chapter_15/Snowflake_scripts/RBAC_setup.sql
+++ Chapter_15/Snowflake_scripts/RBAC_setup.sql
@@ -1,5 +1,6 @@
 use role SYSADMIN;
 create warehouse if not exists BAKERY_WH with warehouse_size = 'XSMALL';
+use warehouse BAKERY_WH;
 create database if not exists BAKERY_DB;
 use database BAKERY_DB;
 
