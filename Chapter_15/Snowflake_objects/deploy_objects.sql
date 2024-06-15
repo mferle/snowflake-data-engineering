c502f8832c95906c4d67b17ade4f0d5fd1101d40
Maja Ferle <maja.ferle@in516ht.com>
2024-06-15 19:02:40 +0200

    deploy

--- Chapter_15/Snowflake_objects/deploy_objects.sql
+++ Chapter_15/Snowflake_objects/deploy_objects.sql
@@ -10,4 +10,4 @@ execute immediate from '.databases/bakery_db/schemas/ext/streams/Create_JSON_ORD
 
 
 -- snow sql -q "alter git repository ADMIN_DB.GIT_INTEGRATION.SF_DE_IA fetch"
-snow sql -q "execute immediate from @SF_DE_IA/branches/wip/Chapter_15/Snowflake_objects/deploy_objects.sql"
+-- snow sql -q "execute immediate from @SF_DE_IA/branches/wip/Chapter_15/Snowflake_objects/deploy_objects.sql"
