8d8a0b0dc84e0566d20472ccc20b396052b6a3e3
Maja Ferle <maja.ferle@in516ht.com>
2024-06-15 19:17:40 +0200

    deploy

--- Chapter_15/Snowflake_objects/databases/bakery_db/schemas/ext/stages/Create_JSON_ORDERS_STAGE.sql
+++ Chapter_15/Snowflake_objects/databases/bakery_db/schemas/ext/stages/Create_JSON_ORDERS_STAGE.sql
@@ -1,6 +1,7 @@
+--!jinja2
+
 -- create an external stage named JSON_ORDERS_STAGE using the PARK_INN_INTEGRATION as described in Chapter 4
 -- be sure to create the external stage with the JSON file format, eg. file_format = (type = json)
 
 -- quick and dirty if you don't have the storage integration object: create an internal stage
-use database bakery_db;
 create or replace stage EXT.JSON_ORDERS_STAGE file_format = (type = json);
--- Chapter_15/Snowflake_objects/deploy_objects.sql
+++ Chapter_15/Snowflake_objects/deploy_objects.sql
@@ -1,3 +1,4 @@
+use database {{curr_db_name}};
 -- EXT schema
 execute immediate from '../Snowflake_objects/databases/bakery_db/schemas/ext/stages/Create_JSON_ORDERS_STAGE.sql';
 execute immediate from '../Snowflake_objects/databases/bakery_db/schemas/ext/tables/Create_JSON_ORDERS_EXT.sql';
@@ -10,4 +11,4 @@ execute immediate from '../Snowflake_objects/databases/bakery_db/schemas/ext/str
 
 
 -- snow sql -q "alter git repository ADMIN_DB.GIT_INTEGRATION.SF_DE_IA fetch"
--- snow sql -q "execute immediate from @SF_DE_IA/branches/wip/Chapter_15/Snowflake_objects/deploy_objects.sql"
+-- snow sql -q "execute immediate from @SF_DE_IA/branches/wip/Chapter_15/Snowflake_objects/deploy_objects.sql using (curr_db_name => 'BAKERY_DB')"
