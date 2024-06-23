a923bb4bac5c78d67162a3a298dc976c65186db9
Maja Ferle <maja.ferle@in516ht.com>
2024-06-23 14:31:12 +0200

    Chapter 15

--- Chapter_15/Chapter_15_Part3_execute_RBAC.sql
+++ Chapter_15/Chapter_15_Part3_execute_RBAC.sql
@@ -8,7 +8,13 @@ alter git repository SF_DE_IA fetch;
 execute immediate from @SF_DE_IA/branches/main/Chapter_15/Chapter_15_Part2_RBAC.sql;
 
 -- grant the functional roles to the users who perform those business functions
--- in this exercise we grant both functional roles to our current user to be able to test them
-
+-- in this exercise we grant both functional roles to ourself to be able to test them
+use role USERADMIN;
 grant role DATA_ENGINEER to user <your username>;
 grant role DATA_ANALYST to user <your username>;
+
+-- switch to the DATA_ENGINEER role and verify that you can list files from the Git repository stage
+use role DATA_ENGINEER;
+use database ADMIN_DB;
+use schema GIT_INTEGRATION;
+ls @SF_DE_IA/branches/main;
